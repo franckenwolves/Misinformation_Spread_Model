efbdcea9faf88439607d6c01cf7352178a4f1374
jnorris <jeffrey.norris@louisville.edu>
2022-09-27 09:42:31 -0400

    Run.py changed

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
 from virus_on_network.server import server
+import sys
+import socketserver
 
-server.launch()
+for i in range(8521, 8524):
+    try:
+        server.launch(i)
+        port = i
+    except:
+        print(i, " is occupied")
